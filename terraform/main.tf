0c80c1296c592777979a5ed3d3f1c42e267ae652
johntoby <johntoby007@gmail.com>
2024-10-20 20:26:25 +0100

    update to main.tf files

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,9 +86,9 @@ resource "aws_eks_node_group" "example" {
   subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = 2
+    max_size     = 5
+    min_size     = 2
   }
   instance_types = ["t2.medium"]
 
