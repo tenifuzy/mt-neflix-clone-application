0c1a36a05104dcafa6b7aded7db4d3379f266870
Olafusi Teniola <152807406+tenifuzy@users.noreply.github.com>
2024-11-05 23:23:47 +0000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jt-terraform-backend-bucket" # Replace with your own bucket
+    bucket = "ola-terraform-backend-bucket" # Replace with your own bucket
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
     }
